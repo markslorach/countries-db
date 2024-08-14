ede912dbe9828613ceda6efe696dcd3b6fa838be
Mark Slorach <hello@markslorach.com>
2024-08-14 22:42:40 +0100

    loading.tsx

---
+++ app/country/[cca3]/loading.tsx
@@ -0,0 +1,9 @@
+import BackButton from "@/app/components/shared/BackButton";
+
+export default function Loading() {
+  return (
+    <div className="px-4 md:container mt-10">
+      <BackButton />
+    </div>
+  );
+}
