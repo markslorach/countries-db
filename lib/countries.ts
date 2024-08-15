2610a2f0c0896cd5ddc3e5a8cd1772557d3044fd
Mark Slorach <hello@markslorach.com>
2024-08-15 11:52:54 +0100

    axios to fetch for caching

--- lib/countries.ts
+++ lib/countries.ts
@@ -1,24 +1,23 @@
-import axios from "axios";
-
-// Get all countries
 export async function getCountries() {
   try {
-    const response = await axios.get("https://restcountries.com/v3.1/all");
-    const data = response.data;
+    const response = await fetch("https://restcountries.com/v3.1/all", {
+      next: { revalidate: 3600 },
+    });
+    const data = await response.json();
     return { data };
   } catch (error) {
     return { error: "Error fetching data" };
   }
 }
 
-// Get a single country by its cca3 code
 export async function getCountry(cca3: string) {
   try {
-    const response = await axios.get(
-      `https://restcountries.com/v3.1/alpha/${cca3}`
+    const response = await fetch(
+      `https://restcountries.com/v3.1/alpha/${cca3}`,
+      { next: { revalidate: 3600 } }
     );
-    const data = response.data[0];
-    return { data };
+    const data = await response.json();
+    return { data: data[0] };
   } catch (error) {
     return { error: "Error fetching data" };
   }
