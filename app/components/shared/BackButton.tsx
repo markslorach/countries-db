885cde25108187996b5aa2a534deca0ddd682bf8
Mark Slorach <hello@markslorach.com>
2024-08-16 11:07:30 +0100

    referer not consistant - reverted back to home link while looking for solution

--- app/components/shared/BackButton.tsx
+++ app/components/shared/BackButton.tsx
@@ -1,31 +1,31 @@
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
-import { headers } from "next/headers";
+// import { headers } from "next/headers";
 import Link from "next/link";
 
 const BackButton = () => {
-  const headersList = headers();
-  const referer = headersList.get("referer");
+  // const headersList = headers();
+  // const referer = headersList.get("referer");
 
-  let href = "/";
+  // let href = "/";
 
-  if (
-    referer === "http://localhost:3000/favourite-countries" ||
-    referer === "https://countriesdb.vercel.app/favourite-countries"
-  ) {
-    href = "/favourite-countries";
-  } else if (referer === null || referer === "null") {
-    href = "/";
-  }
+  // if (
+  //   referer === "http://localhost:3000/favourite-countries" ||
+  //   referer === "https://countriesdb.vercel.app/favourite-countries"
+  // ) {
+  //   href = "/favourite-countries";
+  // } else if (referer === null || referer === "null") {
+  //   href = "/";
+  // }
 
   return (
-    <Link href={href}>
+    <Link href={"/"}>
       <Button
         variant="outline"
         className="shadow-sm h-14 rounded-lg dark:bg-gray-700 dark:border-gray-500/50"
       >
         <ArrowLeft className="mr-1.5 w-5 h-5" />
-        {href === "/" ? "Home" : "Back to Favourites"}
+        Home
       </Button>
     </Link>
   );
--- app/country/[cca3]/page.tsx
+++ app/country/[cca3]/page.tsx
@@ -8,6 +8,8 @@ import { notFound } from "next/navigation";
 import { getFavouriteCountries } from "@/lib/user";
 import { auth } from "@clerk/nextjs/server";
 
+export const revalidate = 0
+
 const CountryPage = async ({ params }: { params: { cca3: string } }) => {
   const { userId } = auth();
 
