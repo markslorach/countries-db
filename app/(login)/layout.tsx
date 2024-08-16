d876a7334691e88605e7098aeaaa458bcf37f420
Mark Slorach <hello@markslorach.com>
2024-08-16 16:28:38 +0100

    removed sign in modal top margin

--- app/(login)/layout.tsx
+++ app/(login)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const LoginLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="px-4 md:container flex justify-center mt-10">
+    <div className="flex justify-center">
       {children}
     </div>
   );
