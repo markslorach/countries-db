5a5ee1669e101eed3029b1f35c92eb7d80af808f
Mark Slorach <hello@markslorach.com>
2024-08-16 10:36:45 +0100

    dynamic back button set to previous url

--- app/components/shared/BackButton.tsx
+++ app/components/shared/BackButton.tsx
@@ -1,13 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
+import { headers } from "next/headers";
 import Link from "next/link";
 
 const BackButton = () => {
+  const headersList = headers();
+  const referer = headersList.get("referer");
+
+  let href = "/";
+
+  if (referer === "http://localhost:3000/favourite-countries") {
+    href = "/favourite-countries";
+  } else if (referer === null || referer === "null") {
+    href = "/";
+  }
+
   return (
-    <Link href="/">
-      <Button variant="outline" className="shadow-sm h-14 rounded-lg dark:bg-gray-700 dark:border-gray-500/50">
+    <Link href={href}>
+      <Button
+        variant="outline"
+        className="shadow-sm h-14 rounded-lg dark:bg-gray-700 dark:border-gray-500/50"
+      >
         <ArrowLeft className="mr-1.5 w-5 h-5" />
-        Home
+        {href === "/" ? "Home" : "Back to Favourites"}
       </Button>
     </Link>
   );
