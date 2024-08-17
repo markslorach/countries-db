618095305a60179ea70d34041c30a59cae87056c
Mark Slorach <hello@markslorach.com>
2024-08-17 09:54:22 +0100

    next image for country cards

--- app/components/CountryCard.tsx
+++ app/components/CountryCard.tsx
@@ -1,17 +1,21 @@
 import Link from "next/link";
 import { Country } from "../_types/types";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import Image from "next/image";
 
 const CountryCard = ({ country }: { country: Country }) => {
   return (
     <Link href={`/country/${country.cca3}`}>
       <article className="p-5 rounded-lg shadow-sm bg-white dark:bg-gray-700 space-y-7 border border-gray-300/50 dark:border-gray-500/50 md:hover:scale-[104%] transition-transform">
-        <div className="aspect-video">
-          <img
-            className="object-cover w-full h-full rounded-lg shadow-sm"
+        <AspectRatio ratio={16 / 9}>
+          <Image
+            width={300}
+            height={200}
             src={country.flags.png}
             alt={country.name.common}
+            className="object-cover w-full h-full rounded-lg shadow-sm"
           />
-        </div>
+        </AspectRatio>
 
         <div className="space-y-2">
           <h2 className="font-semibold text-lg line-clamp-1">
