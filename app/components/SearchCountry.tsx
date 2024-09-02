93eb6389ceb5828d53745691112bdf076b92e0dc
Mark Slorach <hello@markslorach.com>
2024-09-02 13:27:25 +0100

    added search icon to search country input

--- app/components/SearchCountry.tsx
+++ app/components/SearchCountry.tsx
@@ -1,15 +1,17 @@
 import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 
 type Props = {
-  search: string; 
+  search: string;
   setSearch: (search: string) => void;
 };
 
 const SearchCountry = ({ search = "", setSearch = () => {} }: Props) => {
   return (
-    <div role="search" className="w-full">
+    <div role="search" className="w-full relative flex items-center">
+      <Search className="absolute ml-3 h-6 w-6 text-gray-600" />
       <Input
-        className="focus-visible:ring-0 md:w-72 h-14 px-5 shadow-sm dark:bg-gray-700"
+        className="focus-visible:ring-0 md:w-72 h-14 pl-11 pr-5 shadow-sm dark:bg-gray-700"
         type="text"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,40 +1,25 @@
 import * as React from "react";
+
 import { cn } from "@/lib/utils";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  icon?: React.ReactElement;
-  iconPosition?: "left" | "right";
-}
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, icon, iconPosition = "left", ...props }, ref) => {
+  ({ className, type, ...props }, ref) => {
     return (
-      <div className="relative flex items-center">
-        {icon && (
-          <div
-            className={cn(
-              "absolute inset-y-0 flex items-center pointer-events-none",
-              iconPosition === "right" ? "left-3" : "right-3"
-            )}
-          >
-            {icon}
-          </div>
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-slate-200 dark:border-gray-500/50 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-300 dark:focus-visible:ring-slate-300",
+          className
         )}
-        <input
-          type={type}
-          className={cn(
-            "flex h-10 w-full rounded-md border border-slate-200 dark:border-gray-500/50 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-300 dark:focus-visible:ring-slate-300",
-            icon && (iconPosition === "left" ? "pl-10" : "pr-10"), // Adjusted padding
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
+        ref={ref}
+        {...props}
+      />
     );
   }
 );
-
 Input.displayName = "Input";
 
 export { Input };
