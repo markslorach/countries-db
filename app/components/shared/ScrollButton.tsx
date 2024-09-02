b2c22416d46bdd64f3a4794772bc936302edaaca
Mark Slorach <hello@markslorach.com>
2024-09-02 12:25:10 +0100

    rounded scroll to top button

--- app/components/shared/ScrollButton.tsx
+++ app/components/shared/ScrollButton.tsx
@@ -32,7 +32,7 @@ const ScrollButton = () => {
 
   return (
     <Button
-      className={`shadow-sm fixed sm:bottom-10 sm:right-10 bottom-4 right-4 p-0 h-10 w-10 ${
+      className={`shadow-sm rounded-full fixed sm:bottom-10 sm:right-10 bottom-4 right-4 p-0 h-10 w-10 ${
         visible ? "" : "hidden"
       }`}
       onClick={scrollToTop}
