722d8fe1fd9342d0b370f89046b885ad8eb441c6
Mark Slorach <hello@markslorach.com>
2025-04-11 10:02:34 +0100

    Blue stars

--- app/components/user/favourite-btn.tsx
+++ app/components/user/favourite-btn.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import { useFavouriteCountriesContext } from "@/hooks/useFavouriteCountriesContext";
 import { cn } from "@/lib/utils";
-import { Star } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import { Country } from "@/types/country";
+import { Star } from "lucide-react";
 
 const FavouriteButton = ({ country }: { country: Country }) => {
   const { favouriteCountries, addFavouriteCountry, removeFavouriteCountry } =
@@ -29,14 +29,14 @@ const FavouriteButton = ({ country }: { country: Country }) => {
     <form action={action}>
       <Button
         onClick={handleClick}
-        className="group bg-gray-50 shadow-none transition-colors hover:bg-gray-100/70"
+        className="group bg-gray-100/70 shadow-none transition-colors hover:bg-gray-100/70"
         size="icon"
         type="submit"
       >
         <Star
           className={cn("size-6", {
-            "fill-yellow-400 text-yellow-400": isFavourite,
-            "text-gray-300": !isFavourite,
+            "fill-blue-400 text-blue-400": isFavourite,
+            "text-gray-400/70": !isFavourite,
           })}
         />
       </Button>
