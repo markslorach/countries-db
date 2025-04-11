992424a2dbdc992a2fd1902feefd628b6e192963
Mark Slorach <hello@markslorach.com>
2025-04-11 12:31:02 +0100

    Added name and email to account page

--- app/(pages)/account/page.tsx
+++ app/(pages)/account/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth";
 import Container from "@/app/components/shared/container";
 import Heading from "@/app/components/shared/heading";
+import { UserRound, Mail } from "lucide-react";
 
 const AccountPage = async () => {
   const session = await auth.api.getSession({
@@ -16,6 +17,17 @@ const AccountPage = async () => {
   return (
     <Container className="mt-10">
       <Heading className="mb-5 leading-snug">Account </Heading>
+      
+      <div className="flex flex-col gap-2">
+        <span className="flex items-center gap-2">
+          <UserRound className="size-6 text-gray-400" />
+          {session.user?.name}
+        </span>
+        <span className="flex items-center gap-2">
+          <Mail className="size-6 text-gray-400" />
+          {session.user?.email}
+        </span>
+      </div>
     </Container>
   );
 };
