802edafe8649704758c8111b0eba6eae92b59348
Mark Slorach <hello@markslorach.com>
2025-04-11 22:17:10 +0100

    Updated zod validation schema rules

--- utils/validationSchemas.ts
+++ utils/validationSchemas.ts
@@ -1,19 +1,41 @@
 import { z } from "zod";
 
 export const signInFormSchema = z.object({
-  email: z.string().email("Please enter a valid email address"),
-  password: z.string().min(8, "Password must be at least 8 characters long"),
+  email: z
+    .string()
+    .email("Please enter a valid email address")
+    .min(1, "Email is required")
+    .max(100, "Email must be at most 100 characters long"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(50, "Password must be at most 50 characters long"),
 });
 
-export const signUpFormSchema = z.object({
-  email: z.string().email("Please enter a valid email address"),
-  password: z.string().min(8, "Password must be at least 8 characters long"),
-  confirmPassword: z.string().min(8, "Please confirm your password"),
-  name: z.string().min(1, "Name is required"),
-}).refine((data) => data.password === data.confirmPassword, {
-  message: "Passwords don't match",
-  path: ["confirmPassword"],
-});
+export const signUpFormSchema = z
+  .object({
+    email: z
+      .string()
+      .email("Please enter a valid email address")
+      .min(1, "Email is required")
+      .max(100, "Email must be at most 100 characters long"),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .max(50, "Password must be at most 50 characters long"),
+    confirmPassword: z
+      .string()
+      .min(8, "Please confirm your password")
+      .max(50, "Please confirm your password"),
+    name: z
+      .string()
+      .min(1, "Name is required")
+      .max(50, "Name must be at most 50 characters long"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
 
 export type SignInFormType = z.infer<typeof signInFormSchema>;
 export type SignUpFormType = z.infer<typeof signUpFormSchema>;
