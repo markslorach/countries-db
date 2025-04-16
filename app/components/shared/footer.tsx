779668b295423ef10c12cc6463fc438c826c50a6
Mark Slorach <hello@markslorach.com>
2025-04-16 14:19:33 +0100

    Updated footer

--- app/components/shared/footer.tsx
+++ app/components/shared/footer.tsx
@@ -1,29 +1,67 @@
 import Link from "next/link";
 import { Github } from "lucide-react";
 import Container from "./container";
+import Heading from "./heading";
 
 const Footer = () => {
   return (
-    <footer className="h-20 border-t">
-      <Container className="flex items-center justify-between">
-        <p className="text-sm text-gray-500">
-          Created by{" "}
-          <Link
-            href="https://markslorach.com"
-            target="_blank"
-            className="text-blue-500 underline underline-offset-3"
-          >
-            Mark Slorach
-          </Link>
-          .
-        </p>
+    <footer className="border-t py-7">
+      <Container className="flex flex-col space-y-6">
+        <div className="grid w-full grid-cols-2 border-b pb-6 sm:flex sm:w-fit sm:gap-18">
+          <div>
+            <Heading tag="h3" className="mb-2 text-sm font-medium">
+              Data Sources
+            </Heading>
+            <ul className="text-muted-foreground space-y-1 text-sm">
+              <li>
+                <Link
+                  href="https://restcountries.com"
+                  className="underline-offset-2 hover:underline"
+                  target="_blank"
+                >
+                  REST Countries API
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://flagpedia.net"
+                  className="underline-offset-2 hover:underline"
+                  target="_blank"
+                >
+                  Flagpedia
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-        <Link
-          href="https://github.com/markslorach/countries-db.git"
-          target="_blank"
-        >
-          <Github className="size-5" />
-        </Link>
+          <div>
+            <Heading tag="h3" className="mb-2 text-sm font-medium">
+              Source Code
+            </Heading>
+            <Link
+              href="https://github.com/markslorach/countries-db"
+              className="text-muted-foreground flex items-center gap-1 text-sm underline-offset-2 hover:underline"
+              target="_blank"
+            >
+              <Github className="size-4" />
+              <span>GitHub</span>
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <p className="text-muted-foreground text-[13px]">
+            Created by{" "}
+            <Link
+              href="https://markslorach.com"
+              className="text-blue-500 underline underline-offset-2"
+              target="_blank"
+            >
+              Mark Slorach
+            </Link>
+            .
+          </p>
+        </div>
       </Container>
     </footer>
   );
