e4b6f80a22a233ef03cf153321d487acb3e1c8e6
Mark Slorach <hello@markslorach.com>
2025-04-17 13:36:38 +0100

    Added loading to Google Sign In button and extracted into handleGoogleSignIn function

--- app/(pages)/auth/google-sign-in.tsx
+++ app/(pages)/auth/google-sign-in.tsx
@@ -2,21 +2,30 @@
 import { Button } from "@/components/ui/button";
 import GoogleLogo from "@/components/ui/google-logo";
 import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 const GoogleSignIn = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGoogleSignIn = async () => {
+    setIsLoading(true);
+
+    await authClient.signIn.social({
+      provider: "google",
+    });
+
+    setIsLoading(false);
+  };
+
   return (
     <Button
-      onClick={async () =>
-        await authClient.signIn.social({
-          provider: "google",
-        })
-      }
+      onClick={handleGoogleSignIn}
       variant="outline"
       className="h-11 flex-1 font-semibold"
       type="button"
     >
       <GoogleLogo />
-      Google
+      {isLoading ? "Loading...." : "Google"}
     </Button>
   );
 };
