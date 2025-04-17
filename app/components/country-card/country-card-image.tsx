0053ff0a239f347b0e55ac7af5050e42e9d09a16
Mark Slorach <hello@markslorach.com>
2025-04-17 18:05:55 +0100

    Added fallback image for country card flag

--- app/components/country-card/country-card-image.tsx
+++ app/components/country-card/country-card-image.tsx
@@ -12,8 +12,10 @@ const CountryCardImage = ({ country }: CountryCardImageProps) => {
       <Image
         width={300}
         height={200}
-        //  TODO: Add fallback image
-        src={country.flags.svg || country.flags.png || ""}
+        priority
+        src={
+          country.flags.svg || country.flags.png || "/images/no-image-found.png"
+        }
         alt={`Flag of ${country.name.common}`}
         className="h-full w-full rounded-xs border border-gray-300/50 object-contain shadow-xs dark:border-gray-500/50"
       />
