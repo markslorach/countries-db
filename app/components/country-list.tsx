a88e990eaeffa22fc0dbdc1b6e1973defa018127
Mark Slorach <hello@markslorach.com>
2025-04-21 21:05:07 +0100

    Cleaned up filtering logic

--- app/components/country-list.tsx
+++ app/components/country-list.tsx
@@ -10,30 +10,29 @@ type CountryListProps = {
 const CountryList = ({ countries }: CountryListProps) => {
   const { debouncedSearchQuery, selectedRegion, sortDirection } = useFilter();
 
-  const filteredCountries = countries?.filter((country) => {
-    const matchesSearch = country.name.common
-      .toLowerCase()
-      .includes(debouncedSearchQuery.toLowerCase());
+  const displayedCountries = countries
+    .filter((country) =>
+      country.name.common
+        .toLowerCase()
+        .includes(debouncedSearchQuery.toLowerCase()),
+    )
+    .filter(
+      (country) =>
+        selectedRegion === "All" || country.region === selectedRegion,
+    )
+    .sort((a, b) =>
+      sortDirection === "asc"
+        ? a.name.common.localeCompare(b.name.common)
+        : b.name.common.localeCompare(a.name.common),
+    );
 
-    const matchesRegion =
-      selectedRegion === "All" || country.region === selectedRegion;
-
-    return matchesSearch && matchesRegion;
-  });
-
-  const sortedCountries = filteredCountries?.sort((a, b) => {
-    return sortDirection === "asc"
-      ? a.name.common.localeCompare(b.name.common)
-      : b.name.common.localeCompare(a.name.common);
-  });
-
-  if (sortedCountries?.length === 0) {
+  if (displayedCountries.length === 0) {
     return <p className="text-gray-500">No countries found.</p>;
   }
 
   return (
     <section className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {sortedCountries?.map((country) => (
+      {displayedCountries.map((country) => (
         <CountryCard key={country.cca3} country={country as Country} />
       ))}
     </section>
